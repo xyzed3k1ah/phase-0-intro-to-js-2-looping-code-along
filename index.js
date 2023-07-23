7070794f37c39865387a69b514fcf7715ed6c127
Zedikiah <xyzed3k1ah@gmail.com>
2023-07-22 20:46:44 -0600

    Loops Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let message = [ 'Guadalupe', 'Ollie', 'Aki' ];
+
+function writeCards(message) {
+    let result = []
+  for (let i = 0; i < message.length; i++) {
+    result.push(`Thank you, ${message[i]}, for the wonderful surprise gift!`);
+    debugger;
+  }
+
+  return result;
+}
+
+function countDown() {
+    let countDown = 0; 
+    while (countDown < 11) {
+      console.log(countDown++);
+    }
+  
+}
+  
+ 
